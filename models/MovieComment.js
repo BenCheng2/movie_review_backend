8c7ee2f06ebe7b5af80d730dc3029c8d90a07e8f
BenCheng2 <bencheng0609@gmail.com>
2023-07-20 21:34:05 -0400

    feat(Feature): Add Movie Comment Model

---
+++ models/MovieComment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const movieCommentSchema = new mongoose.Schema({
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        movieId: {
+            // IMDB ID
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('MovieComment', movieCommentSchema);
